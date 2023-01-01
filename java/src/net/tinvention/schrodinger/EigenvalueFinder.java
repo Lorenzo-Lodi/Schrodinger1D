8495c32c772f2c5ddbd0fcbb86a57c54a1cff871
Lorenzo-Lodi <lodilory@gmail.com>
2023-01-01 18:48:25 +0100

    wip on oo conversion

--- java/src/net/tinvention/schrodinger/EigenvalueFinder.java
+++ java/src/net/tinvention/schrodinger/EigenvalueFinder.java
@@ -4,6 +4,8 @@ import net.tinvention.schrodinger.grid.UniformGrid;
 import net.tinvention.schrodinger.potential.Potential;
 
 public class EigenvalueFinder {
+	private static final double mass = 2.0d;
+	private static final double targetRelativeError = Math.ulp(1.d); // change for single-precision float
 	UniformGrid grid;
 	Potential v;
 
@@ -11,7 +13,7 @@ public class EigenvalueFinder {
 		this.grid = grid;
 		this.v = v;
 	}
-	
+
 	public EnergyLevel computeApproximateEnergyLevel(int nOfDesiredNodes) {
 		EnergyLevel result = new EnergyLevel();
 
@@ -38,5 +40,55 @@ public class EigenvalueFinder {
 
 		return result;
 	}
-	
+
+	public double findEigenvalue(int nOfDesiredNodes, UniformGrid grid, Potential v) {
+
+		EnergyLevel trial = this.computeApproximateEnergyLevel(nOfDesiredNodes);
+
+		double e = trial.energy;
+		double eLow = trial.lowerBound;
+		double eHigh = trial.upperBound;
+
+		// now we can bisect the energy
+		int imax = 100; // maximum of 100 bisection, reduces by 2**100
+		for (int i = 1; i <= imax; i++) {
+			e = (eHigh + eLow) * 0.5d;
+			if ((eHigh - eLow) / Math.abs(e) < targetRelativeError) {
+				break;
+			}
+			if (countNodes(e) > nOfDesiredNodes) {
+				eHigh = e;
+			} else {
+				eLow = e;
+			}
+		}
+		return e;
+	}
+
+	private int countNodes(double e) {
+
+		// first (leftmost) point
+		double x = grid.xmin;
+		double f0 = 0.d;
+
+		// second point
+		x += grid.h;
+		double f1 = 0.0000001d; // arbitrary initial value
+		int nOfNodes = 0;
+		while (true) {
+			x = x + grid.h;
+			double f2 = 2.0d * f1 * (1.0d - grid.h * grid.h * mass * (e - v.value(x))) - f0;
+			if (f1 * f2 <= 0.d) {
+				nOfNodes++;
+			}
+			f0 = f1;
+			f1 = f2;
+			if (x >= grid.xmax) {
+				break;
+			}
+		}
+
+		return nOfNodes;
+	}
+
 }
--- java/src/net/tinvention/schrodinger/Main.java
+++ java/src/net/tinvention/schrodinger/Main.java
@@ -5,7 +5,6 @@ import net.tinvention.schrodinger.potential.HarmonicPotential;
 import net.tinvention.schrodinger.potential.Potential;
 
 public class Main {
-	private static double mass = 2.0d;
 
 	public static void main(String[] args) {
 
@@ -17,60 +16,10 @@ public class Main {
 			EigenvalueFinder finder = new EigenvalueFinder(grid, v);
 
 			for (int nOfDesiredNodes = 0; nOfDesiredNodes < 1; nOfDesiredNodes++) {
-				EnergyLevel trial = finder.computeApproximateEnergyLevel(nOfDesiredNodes);
-				double ek = findEigenvalue(nOfDesiredNodes, trial.lowerBound, trial.upperBound, trial.energy, grid, v);
+				double ek = finder.findEigenvalue(nOfDesiredNodes, grid, v);
 				System.out.println(i + " " + grid.h + " " + ek);
 			}
 		}
-	}
-
-	private static double findEigenvalue(int nOfDesiredNodes, double eLowInput, double eHighInput, double eTrial,
-			UniformGrid grid, Potential v) {
-		double target_relative_error = Math.ulp(1.d); // change for single-precision float
-		double e = eTrial;
-		double eLow = eLowInput;
-		double eHigh = eHighInput;
-
-		// now we can bisect the energy
-		int imax = 100; // maximum of 100 bisection, reduces by 2**100
-		for (int i = 1; i <= imax; i++) {
-			e = (eHigh + eLow) * 0.5d;
-			if ((eHigh - eLow) / Math.abs(e) < target_relative_error) {
-				break;
-			}
-			if (countNodes(grid, e, v) > nOfDesiredNodes) {
-				eHigh = e;
-			} else {
-				eLow = e;
-			}
-		}
-		return e;
-	}
-
-	private static int countNodes(UniformGrid grid, double e, Potential v) {
-
-		// first (leftmost) point
-		double x = grid.xmin;
-		double f0 = 0.d;
-
-		// second point
-		x += grid.h;
-		double f1 = 0.0000001d; // arbitrary initial value
-		int nOfNodes = 0;
-		while (true) {
-			x = x + grid.h;
-			double f2 = 2.0d * f1 * (1.0d - grid.h * grid.h * mass * (e - v.value(x))) - f0;
-			if (f1 * f2 <= 0.d) {
-				nOfNodes++;
-			}
-			f0 = f1;
-			f1 = f2;
-			if (x >= grid.xmax) {
-				break;
-			}
-		}
-
-		return nOfNodes;
 	}
 
 }
