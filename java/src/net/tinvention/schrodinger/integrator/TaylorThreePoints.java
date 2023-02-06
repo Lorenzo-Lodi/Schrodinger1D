7037e9c96d44a9068d57f5f7e799ba454e34366c
Lorenzo Lodi <lodilory@gmail.com>
2023-02-06 12:19:10 +0100

    minimal refactoring

--- java/src/net/tinvention/schrodinger/integrator/TaylorThreePoints.java
+++ java/src/net/tinvention/schrodinger/integrator/TaylorThreePoints.java
@@ -3,7 +3,7 @@ package net.tinvention.schrodinger.integrator;
 import net.tinvention.schrodinger.potential.DressedPotential;
 
 public class TaylorThreePoints implements Integrator {
-	public double propagate(double yOfN, double psiOfNMinusOne, double psiOfN, double stepSize, DressedPotential v) {
-		return psiOfN * (2.0d - stepSize * stepSize * v.value(yOfN)) - psiOfNMinusOne;
+	public double propagate(double yOfN, double psiOfNMinusOne, double psiOfN, double stepSize, DressedPotential qTilde) {
+		return psiOfN * (2.0d - stepSize * stepSize * qTilde.value(yOfN)) - psiOfNMinusOne;
 	}
 }
