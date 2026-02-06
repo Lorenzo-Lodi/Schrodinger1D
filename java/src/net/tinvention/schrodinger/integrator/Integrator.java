060a1d27154c42f0e1b12f776be36445d0de1c0b
Lorenzo Lodi <lodilory@gmail.com>
2026-02-07 00:50:44 +0100

    added some docs

--- java/src/net/tinvention/schrodinger/integrator/Integrator.java
+++ java/src/net/tinvention/schrodinger/integrator/Integrator.java
@@ -6,6 +6,8 @@ import net.tinvention.schrodinger.potential.TransformedQFunction;
 public interface Integrator {
 	double propagateForward(double[] psi, int n, TransformedQFunction potential);
 
+	double propagateBackward(double[] psi, int n, TransformedQFunction potential);
+
 	default double computePerturbativeCorrection(EnergyLevel level, TransformedQFunction qTilde) {
 		return 0;
 	}
