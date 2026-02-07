11e78b26b158340387b3b1a041cf02856f17748c
Lorenzo Lodi <lodilory@gmail.com>
2026-02-07 16:46:18 +0100

    added comment

--- java/src/net/tinvention/schrodinger/integrator/Integrator.java
+++ java/src/net/tinvention/schrodinger/integrator/Integrator.java
@@ -4,12 +4,13 @@ import net.tinvention.schrodinger.EnergyLevel;
 import net.tinvention.schrodinger.potential.TransformedQFunction;
 
 public interface Integrator {
-	double propagateForward(double[] psi, int n, TransformedQFunction potential);
+    double propagateForward(double[] psi, int n, TransformedQFunction potential);
 
-	double propagateBackward(double[] psi, int n, TransformedQFunction potential);
+    double propagateBackward(double[] psi, int n, TransformedQFunction potential);
 
-	default double computePerturbativeCorrection(EnergyLevel level, TransformedQFunction qTilde) {
-		return 0;
-	}
+    // TODO computePerturbativeCorrection should probably go into a separate interface. Consider refactoring.
+    default double computePerturbativeCorrection(EnergyLevel level, TransformedQFunction qTilde) {
+        return 0;
+    }
 
 }
