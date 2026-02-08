a9c894a6a02d6ba82ff277720ebeb09eab33bf80
Lorenzo Lodi <lodilory@gmail.com>
2026-02-08 23:31:50 +0100

    small change to java/src/net/tinvention/schrodinger/EnergyLevel.java

--- java/src/net/tinvention/schrodinger/EnergyLevel.java
+++ java/src/net/tinvention/schrodinger/EnergyLevel.java
@@ -3,61 +3,64 @@ package net.tinvention.schrodinger;
 import net.tinvention.schrodinger.grid.Grid;
 
 public class EnergyLevel {
-	public int numberOfNodes;
-	public double energy;
-	public double upperBound;
-	public double lowerBound;
-	public int numberOfBisections;
-	public double[] psi;
-	public double perturbativeCorrectionToEnergy;
-	private final Grid grid;
-	private boolean isPsiNormalized = false;
-	public Integer numberOfNodesUpperBound;
-	public Integer numberOfNodesLowerBound;
+    public int numberOfNodes;
+    public double energy;
+    public double upperBound;
+    public double lowerBound;
+    public int numberOfBisections;
+    public double[] psi;
+    public double perturbativeCorrectionToEnergy;
+    private final Grid grid;
+    private boolean isPsiNormalized = false;
+    public Integer numberOfNodesUpperBound;
+    public Integer numberOfNodesLowerBound;
 
-	public EnergyLevel(Grid grid) {
-		this.grid = grid;
-		this.psi = new double[grid.getNumberOfPoints()];
-	}
+    public EnergyLevel(Grid grid) {
+        this.grid = grid;
+        this.psi = new double[grid.getNumberOfPoints()];
+    }
 
-	// Note: because the wavefunctions are exponentially decreasing (or faster), the
-	// trapezoidal rule is very quickly convergent (exponentially or so) until the
-	// truncation error
-	// at the borders takes over the global error.
-	// The rectangle rule is practically the same.
-	public double normalizePsi() {
-		double sum = 0.0d;
-		for (int i = 1; i < grid.getNumberOfPoints() - 1; i++) {
-			sum += psiTimesGSquared(i);
-		}
-		sum += 0.5 * (psiTimesGSquared(0) + psiTimesGSquared(grid.getNumberOfPoints() - 1));
+    // Note: because the wavefunctions are exponentially decreasing (or faster), the
+    // trapezoidal rule is very quickly convergent (exponentially or so) until the
+    // truncation error
+    // at the borders takes over the global error.
+    // The rectangle rule is practically the same.
+    public double normalizePsi() {
+        // Pre-calculate end points to avoid if-checks or method calls in loop
+        int maxIndex = grid.getNumberOfPoints() - 1;
 
-		sum = sum * grid.getStepSizeYCoordinate();
+        // Trapezoidal rule boundaries
+        double valStart = psi[0] * grid.g(0);
+        double valEnd = psi[maxIndex] * grid.g(maxIndex);
+        double sum = 0.5 * (valStart * valStart + valEnd * valEnd);
 
-		double normalizationFactor = 1. / Math.sqrt(sum);
-		for (int i = 0; i < grid.getNumberOfPoints(); i++) {
-			psi[i] = psi[i] * normalizationFactor;
-		}
-		isPsiNormalized = true;
-		return normalizationFactor;
-	}
+        for (int i = 1; i < maxIndex; i++) {
+            double val = psi[i] * grid.g(i);
+            sum += val * val;
+        }
 
-	private double psiTimesGSquared(int i) {
-		return Math.pow(psi[i] * grid.g(i), 2);
-	}
+        sum = sum * grid.getStepSizeYCoordinate();
+        double normalizationFactor = 1.0 / Math.sqrt(sum);
+        for (int i = 0; i <= maxIndex; i++) {
+            psi[i] *= normalizationFactor;
+        }
 
-	public boolean isPsiNormalized() {
-		return isPsiNormalized;
-	}
-	
-	public int countNumberOfNodes() {
-		int nOfNodes = 0;
-		for (int n = 0; n < grid.getNumberOfPoints() - 1; n++) {
-			if (psi[n] * psi[n + 1] < 0.d) {
-				nOfNodes++;
-			}
-		}
-		return nOfNodes;
-	}
+        isPsiNormalized = true;
+        return normalizationFactor;
+    }
+
+    public boolean isPsiNormalized() {
+        return isPsiNormalized;
+    }
+
+    public int countNumberOfNodes() {
+        int nOfNodes = 0;
+        for (int n = 0; n < grid.getNumberOfPoints() - 1; n++) {
+            if (psi[n] * psi[n + 1] < 0.d) {
+                nOfNodes++;
+            }
+        }
+        return nOfNodes;
+    }
 
 }
