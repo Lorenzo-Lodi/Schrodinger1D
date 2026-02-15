98a4b71dd8b9ce20617150f66ea03d7e5810817b
Lorenzo-Lodi <lodilory@gmail.com>
2026-02-16 00:17:31 +0100

    added numerov method

--- java/src/schrodinger/integrator/Numerov.java
+++ java/src/schrodinger/integrator/Numerov.java
@@ -14,7 +14,14 @@ public class Numerov implements Integrator {
 
     @Override
     public void computePerturbativeCorrection(QuantumState level) {
-        level.perturbativeCorrectionToEnergy = 0;
+        double result = 0;
+        for (int i = 1; i < level.getGrid().getNumberOfPoints() - 1; i++) {
+            result += Math.pow(level.QTildeValueAt(i + 1) * level.psi[i + 1] - level.QTildeValueAt(i - 1) * level.psi[i - 1], 2);
+        }
+        result = result / 4.;
+        double hy = level.getGrid().getStepSizeYCoordinate();
+        result = result * hy * hy * hy / (480.0 * level.getMass());
+        level.perturbativeCorrectionToEnergy = result;
     }
 
 }
