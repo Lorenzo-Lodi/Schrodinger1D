af267fe6bce26d7a0bbfa7f521e7d4b3408a90b0
Lorenzo Lodi <lorenzo.lodi@tinvention.net>
2026-03-05 12:23:54 +0100

    Obrechkoff integrator wip

--- java/src/schrodinger/integrator/Obrechkoff.java
+++ java/src/schrodinger/integrator/Obrechkoff.java
@@ -14,11 +14,27 @@ public class Obrechkoff implements Integrator {
         return 6;
     }
 
+
+    // https://www.perplexity.ai/search/can-an-adams-moulton-method-be-4iA7wTxyTHOyelu30RUUGA#2254f9eb-2c6a-49ad-86e3-c5fbae55a1b0
     @Override
     public double propagate(double[] psi, int n, double step, IntToDoubleFunction qTilde, Direction direction) {
-        double eps = 1e-6;
+        int d = direction.getValue();
         // TODO !!!!
-        return psi[n] * (2.0d - step * step * qTilde.applyAsDouble(n)) - psi[n - direction.getValue()];
+        double Q_prime_n = 0;
+        double Q_prime_prime_n = 0;
+        // TODO !!!!
+
+        double Q_n = qTilde.applyAsDouble(n);
+        double Q_np1 = qTilde.applyAsDouble(n + d);
+        double Q_nm1 = qTilde.applyAsDouble(n - d);
+
+        double h2 = step * step;
+        double h3 = h2 * step;
+        double C_n = 3. * h3 * Q_prime_n / (10. * (6. - h2 * Q_n));
+        double A_n = 1. + (h2 / 30.) * Q_np1 + C_n;
+        double B_n = 2. - (14. / 15.) * h2 * Q_n - C_n * h3 * Q_prime_n / 3. + (h2 * h2 / 20.) * (Q_n * Q_n - Q_prime_prime_n);
+        double E_n = 1. + (h2 / 30.) * Q_nm1 - C_n;
+        return (B_n * psi[n] - E_n * psi[n - d]) / A_n;
     }
 
 }
