a4078b6ab723563e37823d73cbf02a2800ac0b80
Lorenzo Lodi <lodilory@gmail.com>
2026-03-23 11:26:04 +0100

    wip on CF Magnus integrators

--- java/test/schrodinger/integrator/rungekutta/CFMagnus8Test.java
+++ java/test/schrodinger/integrator/rungekutta/CFMagnus8Test.java
@@ -32,7 +32,7 @@ public class CFMagnus8Test extends AbstractIntegratorTest {
 
     @Override
     protected ConvergenceParams convergence_params_exp_minus_x() {
-        return new ConvergenceParams(4.00, 0.003);
+        return new ConvergenceParams(1.00, 1e12);
     }
 
     @Override
