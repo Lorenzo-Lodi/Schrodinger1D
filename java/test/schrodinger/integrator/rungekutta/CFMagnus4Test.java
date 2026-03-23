81932438c2256b70f8113e756ceb7cec408a337a
Lorenzo Lodi <lodilory@gmail.com>
2026-03-23 11:01:26 +0100

    wip on CF Magnus integrators

--- java/test/schrodinger/integrator/rungekutta/CFMagnus4Test.java
+++ java/test/schrodinger/integrator/rungekutta/CFMagnus4Test.java
@@ -1,8 +1,11 @@
 package schrodinger.integrator.rungekutta;
 
+import org.junit.jupiter.api.Test;
 import schrodinger.integrator.AbstractIntegratorTest;
 import schrodinger.integrator.Integrator;
 import schrodinger.integrator.verification.ConvergenceParams;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CFMagnus4Test extends AbstractIntegratorTest {
 
@@ -11,17 +14,17 @@ public class CFMagnus4Test extends AbstractIntegratorTest {
         return new CFMagnus4();
     }
 
-//    @Test
-//    public void integrateForwardReferenceTest() {
-//        double val = integrateOneStep(Integrator.Direction.FORWARD);
-//        assertEquals(0.3996918280374979, val, 1e-14);
-//    }
-//
-//    @Test
-//    public void integrateBackwardReferenceTest() {
-//        double val = integrateOneStep(Integrator.Direction.BACKWARD);
-//        assertEquals(0.336941647798199, val, 1e-14);
-//    }
+    @Test
+    public void integrateForwardReferenceTest() {
+        double val = integrateOneStep(Integrator.Direction.FORWARD);
+        assertEquals(0.7390182569261168, val, 1e-14);
+    }
+
+    @Test
+    public void integrateBackwardReferenceTest() {
+        double val = integrateOneStep(Integrator.Direction.BACKWARD);
+        assertEquals(0.7581169428778928, val, 1e-14);
+    }
 
     @Override
     protected ConvergenceParams convergence_params_exp_to_cos_x() {
