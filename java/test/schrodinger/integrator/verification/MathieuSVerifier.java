e44a1b45ff2415a9633280105f977b90dfa6bd64
Lorenzo Lodi <lodilory@gmail.com>
2026-03-26 12:45:07 +0100

    wip on MathieuSVerifier

--- java/test/schrodinger/integrator/verification/MathieuSVerifier.java
+++ java/test/schrodinger/integrator/verification/MathieuSVerifier.java
@@ -5,6 +5,22 @@ import schrodinger.integrator.Integrator;
 public class MathieuSVerifier extends ManufacturedSolutionVerifier {
     // Solution of y''[x] == -(100 - 60 Cos[2 x]) y[x]
     // The solution is MathieuS[100, 30, x]
+
+    private static double[] sampledx;
+    private static double[] sampledMathieuS;
+
+    private static final double XMIN = 0.;
+    private static final double XMAX = 10.;
+
+    static {
+        final int np = 2561;
+        sampledx = new double[2561];
+        sampledMathieuS = new double[2561];
+
+
+    }
+
+
     public MathieuSVerifier(Integrator integrator) {
 
         super(MathieuSVerifier::MathieuS,
@@ -16,7 +32,10 @@ public class MathieuSVerifier extends ManufacturedSolutionVerifier {
     }
 
     public void propagate(Integrator.Direction direction, ConvergenceParams params) {
-        super.propagate(0., 10, 40, 500, 10, direction, params);
+        int[] points = new int[]{
+                81, 161, 321, 641, 1281, 2561
+        };
+        super.propagate(XMIN, XMAX, points, direction, params);
     }
 
     private static double MathieuS(double x) {
