3bebf91612d44b0ce28240895ba99a2b62c327cd
Lorenzo-Lodi <lodilory@gmail.com>
2026-04-27 12:03:13 +0200

    wip on stability tests for integrators

--- java/test/schrodinger/integrator/StabilityTest.java
+++ java/test/schrodinger/integrator/StabilityTest.java
@@ -1,7 +1,6 @@
 package schrodinger.integrator;
 
 import java.util.function.DoubleUnaryOperator;
-import java.util.function.Predicate;
 
 public class StabilityTest {
 
@@ -13,31 +12,44 @@ public class StabilityTest {
         DoubleUnaryOperator qTildePrime = x -> 0.; // Q'(x)
         DoubleUnaryOperator qTildeDoublePrime = x -> 0.; // Q''(x)
 
-        Integrator integrator = IntegratorFactory.getNumerov();
-        String className = integrator.getClass().getSimpleName();
-        System.out.println(className);
+        for (Integrator integrator : IntegratorFactory.getAll()) {
+            String className = integrator.getClass().getSimpleName();
+            System.out.println(className);
 
 
-        final double xmin = 0.;
-        final double xmax = 40.;
-        final int np = 100;
-        double step = (xmax - xmin) / (np - 1);
-        double[] psi = new double[np];
-        double[] currentPsiPrime = new double[1];
+            final double xmin = 0.;
+            final double xmax = 40.;
+            for (int k = 0; k <= 30; k++) {
+                int np = 50 + 5 * k * k;
+                double step = (xmax - xmin) / (np - 1);
+                double[] psi = new double[np];
+                double[] currentPsiPrime = new double[1];
 
-        DoubleUnaryOperator qTildeI = i -> qTilde.applyAsDouble(xmin + i * step);
-        DoubleUnaryOperator qTildePrimeI = i -> qTildePrime.applyAsDouble(xmin + i * step);
-        DoubleUnaryOperator qTildeDoublePrimeI = i -> qTildeDoublePrime.applyAsDouble(xmin + i * step);
+                DoubleUnaryOperator qTildeI = i -> qTilde.applyAsDouble(xmin + i * step);
+                DoubleUnaryOperator qTildePrimeI = i -> qTildePrime.applyAsDouble(xmin + i * step);
+                DoubleUnaryOperator qTildeDoublePrimeI = i -> qTildeDoublePrime.applyAsDouble(xmin + i * step);
 
-        // Initialize
-        for (int n = 0; n < integrator.minHistoryLength(); n++) {
-            double x = xmin + n * step;
-            psi[n] = f.applyAsDouble(x);
-            currentPsiPrime[0] = fPrime.applyAsDouble(x);
+                // Initialize
+                int n;
+                double x = 0;
+                for (n = 0; n < integrator.minHistoryLength(); n++) {
+                    x = xmin + n * step;
+                    psi[n] = f.applyAsDouble(x);
+                    currentPsiPrime[0] = fPrime.applyAsDouble(x);
+                }
+
+                for (n = integrator.minHistoryLength() - 1; n < np - 1; n++) {
+                    psi[n + 1] = integrator.propagate(psi, currentPsiPrime, n, step,
+                            qTildeI, qTildePrimeI, qTildeDoublePrimeI, Integrator.Direction.FORWARD);
+
+                    x = xmin + n * step;
+                    double ratio = Math.abs(psi[n + 1]) / Math.exp(-x);
+                    if (ratio >= 2.) break;
+                }
+                System.out.println(-Math.log10(step) + " " + x);
+            }
         }
 
-
     }
-
 
 }
