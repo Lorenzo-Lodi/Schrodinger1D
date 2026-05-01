0fd1acbf63cc7c4f65acd7279a23edea4778c6a4
Lorenzo Lodi <lorenzo.lodi@tinvention.net>
2026-05-01 17:08:36 +0200

    wip on MorseMain

--- java/test-integration/morse/MorseMain.java
+++ java/test-integration/morse/MorseMain.java
@@ -22,22 +22,35 @@ public class MorseMain {
 
     public static void main(String[] args) {
 
-        PhysicalPotential potential = new PhysicalPotentialMorse(2.0, 1.5, toHartree(50000.));
+        double a = 1.5;
+        double De = toHartree(50000.);
+
+        PhysicalPotential potential = new PhysicalPotentialMorse(2.0, a, De);
         double xmin = 1.0;
         double xmax = 12.;
-        int nOfPoints = (int) ((xmax - xmin) / 0.01);
-        System.out.println(toInverseCm(potential.value(xmin))  + " " + toInverseCm(potential.value(xmax))  + "  " + nOfPoints);
+        int nOfPoints = (int) ((xmax - xmin) / 0.005);
 
         Grid grid = GridFactory.generateUniformGrid(xmin, xmax, nOfPoints);
         double mass = 100. * UMA_TO_ELECTRON_MASS;
         SchrodingerSystem system = new SchrodingerSystem(potential, mass, grid);
 
-        Integrator integrator = IntegratorFactory.getCFMagnus8();
+        Integrator integrator = IntegratorFactory.getPC8i2();
         ShootingSolver finder = new ShootingSolver(system, integrator);
 
+        double omega0 = a * Math.sqrt(2.0 * De / mass);
+        double xe = omega0 / (4. * De);
+        double A = 1. / xe;
+        int nOfBoundStates = (int) ((A + 1.) * 0.5);
+        System.out.printf("omega0=%20.8f cm-1; xe=%20.8f nOfBoundStates=%10d\n", toInverseCm(omega0), xe, nOfBoundStates);
+
+        System.out.println(toInverseCm(potential.value(xmin)) + " " + toInverseCm(potential.value(xmax)) + "  " + nOfPoints);
+
+        System.out.printf("%10s %20s %20s %20s \n", "n", "calc", "exact", "exact-calc");
         for (int nOfDesiredNodes = 0; nOfDesiredNodes <= 200; nOfDesiredNodes++) {
             QuantumLevel ek = finder.findEigenvalue(nOfDesiredNodes, RefinementStrategy.BISECTION_ONLY);
-            System.out.println(nOfDesiredNodes + " " + toInverseCm(ek.energy));
+            double exact = omega0 * (nOfDesiredNodes + 0.5) * (1. - xe * (nOfDesiredNodes + 0.5));
+            double diff = exact - ek.energy;
+            System.out.printf("%10d %20.8f %20.8f %20.8f\n", nOfDesiredNodes, toInverseCm(ek.energy), toInverseCm(exact), toInverseCm(diff));
         }
 
 
