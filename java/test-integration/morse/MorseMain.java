0cb46d9a1bb5655e399d95198edbf24908e1fcc8
Lorenzo Lodi <lorenzo.lodi@tinvention.net>
2026-05-01 15:04:02 +0200

    added morse potential -- to be tested

--- java/test-integration/morse/MorseMain.java
+++ java/test-integration/morse/MorseMain.java
@@ -25,14 +25,14 @@ public class MorseMain {
         PhysicalPotential potential = new PhysicalPotentialMorse(2.0, 1.5, toHartree(50000.));
         double xmin = 1.0;
         double xmax = 12.;
-        int nOfPoints = (int) ((xmax - xmin) / 0.005);
+        int nOfPoints = (int) ((xmax - xmin) / 0.01);
         System.out.println(toInverseCm(potential.value(xmin))  + " " + toInverseCm(potential.value(xmax))  + "  " + nOfPoints);
 
         Grid grid = GridFactory.generateUniformGrid(xmin, xmax, nOfPoints);
         double mass = 100. * UMA_TO_ELECTRON_MASS;
         SchrodingerSystem system = new SchrodingerSystem(potential, mass, grid);
 
-        Integrator integrator = IntegratorFactory.getNumerov();
+        Integrator integrator = IntegratorFactory.getCFMagnus8();
         ShootingSolver finder = new ShootingSolver(system, integrator);
 
         for (int nOfDesiredNodes = 0; nOfDesiredNodes <= 200; nOfDesiredNodes++) {
